02ab8e5a950c1f30957e11b822862421aa271c4b
Giovanni Braga <giovanni.bragadev@outlook.com>
2023-01-16 12:08:52 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const decisao = prompt('Quer namorar comigo? \n1 - Sim\n2 - Não')
+
+if (decisao === 'Sim') {
+  alert("Boa escolha, vou preparar as alianças")
+} else {
+  alert('Ta chapano? Errou, Responde denovo!!!')
+}11
